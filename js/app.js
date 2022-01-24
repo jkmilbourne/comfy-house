bf09b717b00128f38b435f208398c726c5415913
Josh <jkmilbourne@gmail.com>
2022-01-24 13:06:11 -0700

    Restructured seed data and updated app.js

--- js/app.js
+++ js/app.js
@@ -44,9 +44,8 @@ class Products {
       // destructure JSON objects to clean array objects
       let products = data.items;
       products = products.map((item) => {
-        const { title, price } = item.fields;
+        const { title, price, image } = item.fields;
         const { id } = item.sys;
-        const image = item.fields.image.fields.file.url;
         return { title, price, id, image };
       });
       return products;
